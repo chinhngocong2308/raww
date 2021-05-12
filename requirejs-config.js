4f8de98daa01b94033e50fef0713b73f47d9bcc5
chinh ngo <chinhngocong2308@gmaill.com>
2021-05-12 11:50:13 +0700

    raww theme

---
+++ requirejs-config.js
@@ -0,0 +1,14 @@
+var config = {
+    paths: {
+        slick: 'js/slick',
+        carousel: 'js/owl.carousel.min'
+    },
+    shim: {
+        slick: {
+            deps: ['jquery']
+        },
+        carousel: {
+            deps: ['jquery']
+        }
+    }
+};
